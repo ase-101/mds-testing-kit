9ffd709ff1161b87dfae1c26940d945a810b69e1
Tabish Khan <tabish.khansfc@gmail.com>
2020-08-18 17:48:40 +0530

    Added Validation Logic Against Given Process

--- mosip-device-service/src/main/java/io/mosip/mds/entitiy/Store.java
+++ mosip-device-service/src/main/java/io/mosip/mds/entitiy/Store.java
@@ -3,7 +3,9 @@ package io.mosip.mds.entitiy;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.imageio.stream.FileImageInputStream;
 
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.mosip.mds.dto.TestManagerGetDto;
 import io.mosip.mds.dto.TestRun;
 import io.mosip.mds.dto.ValidatorDef;
 import io.mosip.mds.dto.getresponse.MasterDataResponseDto;
@@ -19,6 +22,7 @@ import io.mosip.mds.dto.getresponse.TestExtnDto;
 import io.mosip.mds.validator.AuthRequestResponseValidator;
 
 import io.mosip.mds.repository.TestCaseResultRepository;
+import io.mosip.mds.validator.AuthRequestResponseValidator;
 import io.mosip.mds.validator.MandatoryCaptureResponseValidator;
 import io.mosip.mds.validator.MandatoryDeviceInfoResponseValidator;
 import io.mosip.mds.validator.MandatoryDiscoverResponseValidator;
@@ -218,5 +222,20 @@ public class Store {
 			}
 		}
 		return testExtnDtos;
+	}
+    
+	public static List<TestExtnDto> filterTestType(TestManagerGetDto filter,HashMap<String, TestExtnDto> allTests)
+	{
+		List<TestExtnDto> results =  allTests.values().stream().filter(test -> 
+				(isValid(test.processes) && test.processes.contains(filter.process)) && 
+				(isValid(test.biometricTypes) && test.biometricTypes.contains(filter.biometricType)) &&
+				(isValid(test.deviceSubTypes) && test.deviceSubTypes.contains(filter.deviceSubType)) && 
+				( !isValid(test.mdsSpecVersions) || test.mdsSpecVersions.contains(filter.mdsSpecificationVersion ) ))
+		.collect(Collectors.toList());
+		
+		return results;	
+	}
+	private static boolean isValid(List<String> value) {
+		return (value != null && !value.isEmpty());
 	}
 }
